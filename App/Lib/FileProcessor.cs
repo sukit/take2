e7743d3bb3e40d46485352f0dc1d0806a8ed62ec
Tik Arseanrapoj <tarseanrapoj@brandnewday.nl>
2026-04-01 12:25:28 +0700

    process file and write output to console

---
+++ App/Lib/FileProcessor.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+
+namespace App.Lib;
+
+public class FileProcessor
+{
+    private readonly IStringFilter[] _filters =
+    [
+        new RemoveWordWithVowelMidStringFilter(),
+        new RemoveShortStringFilter(),
+        new RemoveWordWithTFilter()
+    ];
+
+    public string Process(string filePath)
+    {
+        if (!File.Exists(filePath))
+        {
+            throw new FileNotFoundException("The specified file does not exist", filePath);
+        }
+
+        var content =  File.ReadAllText(filePath);
+
+        var regex = new Regex(@"\b(\w)+\b", RegexOptions.Compiled);
+        return regex.Replace(
+            content,
+            match => _filters.Aggregate(match.Value, (s, filter) => filter.Apply(s))
+        );
+    }
+}
--- App/Program.cs
+++ App/Program.cs
@@ -1,3 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using App.Lib;
 
-Console.WriteLine("Hello, World!");
+if (args.Length == 0)
+{
+    Console.WriteLine("Please specify input file path");
+    return;
+}
+
+var p = new FileProcessor();
+try
+{
+    var result = p.Process(args[0]);
+    Console.WriteLine(result);
+}
+catch (FileNotFoundException e)
+{
+    Console.WriteLine($"The specified file does not exist: ${e.FileName}");
+    return;
+}
+
+Console.WriteLine("Done");
